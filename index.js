ceae204454139703b9e9b8b31f353e49a80e5966
MemoryCart <fabianignacio.mena@gmail.com>
2023-02-08 21:45:52 -0300

    aqui yo guarde el index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+//CONSTANTES Y VARIABLES
+
+var miVariable = 24;
+//console.log("mi edad es " + miVariable)
+
+
+miVariable = 12
+//console.log('mi edad ahora es' + miVariable)
+
+const miConstante = 3;
+console.log("el valor de mi constante ahora es " + miConstante)
+
+//OPERACIONES CON VARIABLES
+
+var op1 = 2
+var op2 = 3
+
+var resultado = op1 + op2 ; 
+console.log ('resultado vale: ' + resultado )  
+
+//OTRA OPCION DE OPERACIONES CON VARIABLES
+//pueden ser + - / - (-) - *
+
+var resultado = 2 + 2 ; 
+console.log ('resultado vale: ' + resultado )
+
+//CONDICIONADRES
+// =: Asignacion
+// ==: Comparacion
+// ===: 
+let miNumero = 6 
+let miNombre = "David"
+
+//let resultadoPregunta = miNumero == 8
+//console.log(resultadoPregunta)
+
+if (miNumero == "6") {
+    console.log("Si");
+} else {
+    console.log("No tu nombre es David")
+}
