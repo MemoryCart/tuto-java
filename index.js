45ed7289215f641980d4bb2a95c29309132748e3
MemoryCart <fabianignacio.mena@gmail.com>
2023-02-11 00:18:38 -0300

    TUTO JS-variables

--- index.js
+++ index.js
@@ -34,8 +34,42 @@ let miNombre = "David"
 //let resultadoPregunta = miNumero == 8
 //console.log(resultadoPregunta)
 
-if (miNumero == "6") {
-    console.log("Si");
-} else {
-    console.log("No tu nombre es David")
+//if (miNumero >2 || miNombre == "david" ) {
+//    console.log("Si");
+//} else {
+//    console.log("No tu nombre es David")
+//}
+
+//PREGUNTA CON RESPUESTA CON DOBLE FUNCION
+
+//let miNumero2 = 0
+//if(miNumero2 > 0) {
+  //  console.log("Mi numero es positivo")
+//} else if (miNumero2 == 0){
+ //   console.log("Mi numero es zero");
+//}else{
+//    console.log("Mi numero es negativo");
+//}
+
+//BUCLES
+//podemos configurarlo hasta el num que queramos
+
+//let contador = 0
+//while(contador < 5){
+//   console.log("hola a todos")
+
+//    contador = contador + 1
+//}
+
+//for (let i = 1000000; i >= 0; i-- ) {
+ //   console.log(i);
+//}
+
+//FUNCIONES
+
+function saludar(nombre, edad) {
+    console.log ("hola. Mi nombre es : " + nombre)
+    console.log ("hola. Mi edad es : " + edad)
 }
+
+saludar("David", 29)
